27200b9336307a23dd8aa30fb1c85cc228c72c98
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-07-22 17:40:15 +0100

    Add connect and disconnect event handlers

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+var socket = io();
+socket.on("connect", function () {
+  console.log("Connected to server");
+  
+  socket.emit("createEmail", {
+    to: "jen@example.com",
+    text: "Hey. This is Andrew.",
+  });
+});
+socket.on("newEmail", function (email) {
+  console.log("New email", email);
+});
+socket.on("disconnect", function () {
+  console.log("Disconnected from server");
+});
